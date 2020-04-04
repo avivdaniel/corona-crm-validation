1036791e28649c060fdaa19dd8a5881fa39ecbfa
avivdaniel <avivdaniel@gmail.com>
2020-04-04 21:48:38 +0300

    added validation

---
+++ public/scripts.js
@@ -0,0 +1,174 @@
+const API_URL = 'http://localhost:3000';
+const newCustomerForm = document.getElementById('new-customer-form');
+const fullName = document.getElementById('fullName');
+const feedback = document.getElementById('feedback');
+const over18Field = document.getElementById('over18');
+const over18Feedback = document.getElementById('over18-feedback')
+const email = document.getElementById('email');
+const table = document.getElementById('table');
+const thead = document.getElementById('thead');
+const tbody = document.getElementById('customersTableTb')
+
+newCustomerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!validateAge(newCustomerForm)) {
+        console.log('validateAge faild')
+        return;
+    }
+    if (!validateFullName()) {
+        console.log('validate full name faild');
+        return;
+    }
+    if (!isValidEmail()) {
+        console.log('validate email wrong')
+        return;
+    }
+    createCustomer({
+        fullName: newCustomerForm.fullName.value,
+        email: newCustomerForm.email.value,
+        birthDate: newCustomerForm.birthDate.value,
+        notes: newCustomerForm.notes.value,
+    });
+    console.log('user sended');
+    getCustomers();
+});
+
+
+function validateAge(form) {
+    if (!form.over18.checked) {
+        over18Field.classList.add('is-invalid');
+        over18Feedback.innerHTML = 'you nust be over 18!';
+        over18Feedback.classList.add('invalid-feedback');
+        //I was trying to use the function setValid\invalid but its a problem since the feedback isnot a nextelementsibling of the checkbox and i didnt want to change the logic
+        return false;
+    }
+    over18Field.classList.add('is-valid');
+    over18Feedback.innerHTML = '';
+    return true;
+}
+
+//validation functions 
+function validateFullName() {
+    if (checkIfEmpty(fullName)) {
+        return;
+    }
+    if (CheckIfLessThan2Words(fullName)) {
+        return;
+    }
+    if (!checkIfOnlyLetters(fullName)) {
+        return;
+    }
+    return true;
+}
+
+
+function isValidEmail() {
+    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(email.value).toLowerCase())) {
+        setValid(email)
+        return true;
+    }
+    setInvalid(email, 'You must contain a valid email adress')
+    return false;
+}
+
+//helpers functions
+function checkIfEmpty(field) {
+    if (isEmpty(field.value.trim())) {
+        setInvalid(field, `${field.name} must not be empty`);
+        return true; //because its empty
+    } else {
+        setValid(field);
+        return false;
+    }
+}
+
+function isEmpty(value) {
+    if (value === '') {
+        return true;
+    }
+    return false;
+}
+
+function checkIfOnlyLetters(field) {
+    if (/^[a-zA-Z ]*$/.test(field.value)) {
+        setValid(field);
+        return true;
+    } else {
+        setInvalid(field, `${field.name} must contain only letters`);
+        return false;
+    }
+};
+
+function CheckIfLessThan2Words(field) {
+    if (isLessThan2Words(field.value)) {
+        setInvalid(field, 'You must contain at least 2 words')
+        return true;
+    } else {
+        setValid(field);
+        return false;
+    }
+}
+
+function isLessThan2Words(value) {
+    value = value.split(' ');
+    if (value.length < 2) {
+        return true;
+    }
+    return false;
+}
+
+
+function setInvalid(field, message) {
+    field.classList.add('is-invalid');
+    field.nextElementSibling.innerHTML = message;
+    field.nextElementSibling.classList.add('invalid-feedback');
+    return;
+}
+
+function setValid(field) {
+    field.classList.remove('is-invalid');
+    field.classList.add('is-valid');
+    field.nextElementSibling.innerHTML = '';
+    return;
+}
+
+
+function createCustomer(customer) {
+    return fetch(API_URL + '/customer', {
+        method: 'PUT',
+        body: JSON.stringify(customer),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+function getCustomers() {
+    return fetch(API_URL + '/customer')
+        .then(res => res.json())
+        .then(refreshCustomers())
+        .then(data => {
+            data.forEach(customer => {
+                actionsInnerHTML = `
+            <button class="btn btn-sm">
+            <i class="far fa-edit"></i>
+            </button>
+            <button class="btn btn-sm btn-delete">
+            <i class="far fa-trash-alt"></i>
+            </button>`;
+                tbody.innerHTML = tbody.innerHTML + `
+            <tr>
+            <th>${customer.id}</th>
+            <td>${customer.fullName}</td>
+            <td>${customer.email}</td>
+            <td>${customer.birthDate}</td>
+            <td>${customer.created}</td>
+            <td>${actionsInnerHTML}</td>
+            </tr>`
+            });
+        })
+}
+
+function refreshCustomers() {
+    tbody.innerHTML = '';
+}
--- src/server.js
+++ src/server.js
@@ -1,36 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 let customers = [];
 
+//all customers
 app.get('/customer', (req, res) => {
 	res.json(customers);
 });
 
+//create new customers
 app.put('/customer', (req, res) => {
+	const fullName = req.body.fullName;
+	const email = req.body.email;
+	const birthDate = req.body.birthDate;
+	const notes = req.body.notes;
+	if (!fullName.match(/^[a-zA-Z ]*$/) || fullName.split(' ').length < 2 || !email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+		return res.status(400).send();
+	}
 	customers.push({
 		id: customers.length + 1,
-		fullName: req.body.fullName,
-		email: req.body.email,
-		birthDate: req.body.birthDate,
-		notes: req.body.notes
+		fullName: fullName,
+		email: email,
+		birthDate: birthDate,
+		notes: notes,
+		created: new Date().toDateString()
 	});
 	res.status(201).json(customers);
 });
 
+//delete spesific customer
 app.delete('/customer/:id', (req, res) => {
 	const requestedCustomer = customers.find(customer => {
 		return customer.id === parseInt(req.params.id);
 	});
 
-	if(!requestedCustomer) {
+	if (!requestedCustomer) {
 		res.sendStatus(404);
 		return;
 	}
@@ -40,12 +52,13 @@ app.delete('/customer/:id', (req, res) => {
 	res.json(requestedCustomer);
 });
 
+//edit specific customer
 app.post('/customer/:id', (req, res) => {
 	const requestedCustomer = customers.find(customer => {
 		return customer.id === parseInt(req.params.id);
 	});
 
-	if(!requestedCustomer) {
+	if (!requestedCustomer) {
 		res.sendStatus(404);
 		return;
 	}
